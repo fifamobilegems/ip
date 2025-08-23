f9ff306c087bb9e612d1f4d0bf1f146aade73805
fifamobilegems <max.limhy@gmail.com>
2025-08-23 23:34:21 +0800

    Rename chatbot, refactor code, add greet and exit

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# These User Guide
 
 // Update the title above to match the actual product name
 
--- src/main/java/These.java
+++ src/main/java/These.java
@@ -1,10 +1,15 @@
-public class Duke {
+public class These {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+
+        String intro = "Hello! I'm These\n" + "What can I do for you?";
+        String outro = "Bye. Hope to see you again soon!";
         System.out.println("Hello from\n" + logo);
+        System.out.println(intro);
+        System.out.println(outro);
     }
 }
