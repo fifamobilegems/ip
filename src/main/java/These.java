2c425a516207b3262e0d13841f9eb09965b32f78
fifamobilegems <max.limhy@gmail.com>
2025-08-23 23:55:40 +0800

    Add echo method

--- src/main/java/These.java
+++ src/main/java/These.java
@@ -1,4 +1,18 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class These {
+    public static void echo(String input) {
+        Scanner sc = new Scanner(System.in);
+        String outro = "Bye. Hope to see you again soon!";
+        if (Objects.equals(input, "bye")) {
+            System.out.println(outro);
+        } else {
+            System.out.println(input);
+            String next = sc.nextLine();
+            echo(next);
+        }
+    }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -9,7 +23,6 @@ public class These {
         String intro = "Hello! I'm These\n" + "What can I do for you?";
         String outro = "Bye. Hope to see you again soon!";
         System.out.println("Hello from\n" + logo);
-        System.out.println(intro);
-        System.out.println(outro);
+        echo(intro);
     }
 }
